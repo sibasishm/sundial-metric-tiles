d1009716bc8234befe86c671f560334f2476795e
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-28 02:05:41 +0530

    trying to make server proxy work

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	server: {
 		proxy: {
-			'/api': {
-				target: 'https://sundial-fe-interview.vercel.app',
-				changeOrigin: true,
-			},
+			'/api': 'https://sundial-fe-interview.vercel.app',
 		},
 	},
 	plugins: [react()],
